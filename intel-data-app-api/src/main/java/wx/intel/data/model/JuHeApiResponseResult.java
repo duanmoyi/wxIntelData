78c0d0e85064caf4e462154e339f88dc91ff798e
duanmoyi <961120484@qq.com>
2017-01-21 00:52:14 +0800

    2017/1/21 添加聚合平台接口请求模块

---
+++ intel-data-app-api/src/main/java/wx/intel/data/model/JuHeApiResponseResult.java
@@ -0,0 +1,34 @@
+package wx.intel.data.model;
+
+/**
+ * Created by duanz on 2017/1/20.
+ */
+public class JuHeApiResponseResult {
+    private String reason;
+    private JuHeApiDataResult result;
+    private String error_code;
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getError_code() {
+        return error_code;
+    }
+
+    public void setError_code(String error_code) {
+        this.error_code = error_code;
+    }
+
+    public JuHeApiDataResult getResult() {
+        return result;
+    }
+
+    public void setResult(JuHeApiDataResult result) {
+        this.result = result;
+    }
+}
