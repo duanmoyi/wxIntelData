e6896e48613aac55881de9aec4ef630d56860943
duanmoyi <961120484@qq.com>
2017-01-20 22:02:51 +0800

    框架搭建

---
+++ intel-data-app-api/src/test/java/wx/intel/data/IntelDataAppApiApplicationTests.java
@@ -0,0 +1,16 @@
+package wx.intel.data;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IntelDataAppApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
