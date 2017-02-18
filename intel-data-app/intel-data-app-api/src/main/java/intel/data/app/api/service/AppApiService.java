ac78fc7ef5065f0795581b9d5f8b77d06d36836a
duanmoyi <duanzhimin@outlook.com>
2017-02-18 11:31:14 +0800

    优化模块命名和maven配置

---
+++ intel-data-app/intel-data-app-api/src/main/java/intel/data/app/api/service/AppApiService.java
@@ -0,0 +1,26 @@
+package intel.data.app.api.service;
+
+import com.alibaba.fastjson.JSONArray;
+import org.springframework.stereotype.Service;
+import intel.data.app.api.Util.JuHeApiHttpUtil;
+import intel.data.app.api.model.JuHeApiResponseResult;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by duanz on 2017/1/20.
+ */
+@Service
+public class AppApiService<T> {
+
+    public List<T> getApiResult(String url, Class<T> clazz){
+        JuHeApiResponseResult result = JuHeApiHttpUtil.doGet(url);
+        JSONArray dataResult = result.getResult().getData();
+        List<T> dataList = new ArrayList<>();
+        for(int i=0; i<dataResult.size(); i++){
+            dataList.add(dataResult.getObject(i, clazz));
+        }
+        return dataList;
+    }
+}
---
+++ intel-data-util/src/main/java/intel/data/util/HttpUtil.java
@@ -0,0 +1,7 @@
+package intel.data.util;
+
+/**
+ * Created by duanmoyi on 2017/2/18.
+ */
+public class HttpUtil {
+}
