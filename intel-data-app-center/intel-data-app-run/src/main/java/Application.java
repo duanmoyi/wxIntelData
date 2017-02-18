e0b9f510794eaf183941a6f66f575deb26ae4c2d
duanmoyi <duanzhimin@outlook.com>
2017-02-18 11:02:23 +0800

    框架微调

--- intel-data-app-center/intel-data-app-run/src/main/java/Application.java
+++ intel-data-app-center/intel-data-app-run/src/main/java/Application.java
@@ -1,5 +1,3 @@
-package wx.intel.data.app.api;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
